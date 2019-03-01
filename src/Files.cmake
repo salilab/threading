6f4cb6e6910b9943699afb412e2dd2b09ec6363b
saltzberg <saltzberg.salilab.org>
2019-02-28 16:21:15 -0800

    add missing files

---
+++ src/Files.cmake
@@ -0,0 +1,3 @@
+set(pyfiles "")
+set(cppfiles "ConditionalPairRestraint.cpp;LoopPairDistanceRestraint.cpp;SecondaryStructureParsimonyRestraint.cpp;StructureElement.cpp;StructureElementConnectivityRestraint.cpp;StructureElementMover.cpp")
+set(cudafiles "")
