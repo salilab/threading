f2e66f3e852f1735d8612f02698153fc4fd2248d
saltzberg <saltzberg.salilab.org>
2020-06-03 14:21:28 -0700

    Add checks and cleanup code

--- src/LoopPairDistanceRestraint.cpp
+++ src/LoopPairDistanceRestraint.cpp
@@ -42,10 +42,16 @@ double LoopPairDistanceRestraint::calc_sphere_cap_distance(float R, float d, flo
   // The solution to this is quadratic and for values of alpha from -0.5 - pi/2, the
   // plus form will always be the positive distance
   float sina = sin(alpha);
-
-  float dist = -d * sina + sqrt(d*d*(sina*sina - 1 ) + R*R);
+  
+  float dist = -d * sina + sqrt(abs(d*d*(sina*sina - 1 ) + R*R));
   if (dist < 0) { dist = -1*dist;}
-  //std::cout << "     R,d,a,RR,dd: " << R << ", " << d << ", " << alpha << " " << R*R << " " << d*d*(sina*sina - 1 ) << " || " << dist <<std::endl;
+  
+  // For debugging.  If DIST == NaN, this trips.
+  if (dist!=dist){
+   int i=1;  
+   std::cout << "THe sphere cap distance is undefined.  There is some error in the LoopPairDistanceRestraint calculation" << std::endl;
+   //std::cout << "    NANNAN-R,d,a,RR,dd: " << R << ", " << d << ", " << alpha << " " << R*R << " " << d*d*(sina*sina - 1 ) << " " << sina << " || " << dist <<std::endl;
+  }
   return dist;
 }
 
@@ -62,16 +68,17 @@ ParticleIndexes LoopPairDistanceRestraint::get_sequence_residue_particles() cons
 Particles LoopPairDistanceRestraint::get_closest_built_residue_particles(ParticleIndex pi) const
 {
   // TODO: Ensure that input particle is a residue
-
   // create vector for the endpoint particles
   ParticlesTemp endpoints;
   atom::Residue r = atom::Residue(get_model(), pi);
   int ri = r.get_index();
-  //std::cout << "Closest built resis for: " << ri << "| "; 
+  
   // Get the chain of this residue
   atom::Hierarchy res(get_model(), pi);
   atom::Hierarchy seq_chain = res.get_parent();
+  
   // Go forward in sequence space looking for a coordinate that is optimized (max = 1000)
+  // This is very slow, especially for sparsely built models  and should be optimized
   for (unsigned int i=1; i<1000; i++){
     atom::Selection sel(seq_chain);
     sel.set_residue_index(ri + i);
@@ -80,7 +87,6 @@ Particles LoopPairDistanceRestraint::get_closest_built_residue_particles(Particl
     if (nextres.size() == 0){ break;}
     if (core::XYZ(nextres[0]).get_coordinates_are_optimized()){
       endpoints.push_back(nextres[0]);
-      //std::cout << " " << ri+i;
       break;
     } 
   }
@@ -91,7 +97,7 @@ Particles LoopPairDistanceRestraint::get_closest_built_residue_particles(Particl
     sel.set_residue_index(ri + i);
     ParticlesTemp nextres = sel.get_selected_particles();
     // If you hit the end of the chain, then just leave
-    if (nextres.size() == 0){ std::cout << std::endl; break;}
+    if (nextres.size() == 0){ break;}
     if (core::XYZ(nextres[0]).get_coordinates_are_optimized()){
       endpoints.push_back(nextres[0]);
       //std::cout << " " << ri+i << std::endl;   
@@ -102,27 +108,22 @@ Particles LoopPairDistanceRestraint::get_closest_built_residue_particles(Particl
 }
 
 algebra::Vector3D LoopPairDistanceRestraint::get_sphere_cap_center(Particle* P0, Particle* P1, float R0, float R1) const {
-  // First, define the unit vector
-
+  // First, define the unit vector connecting the two centers
   algebra::Vector3D Dvec = core::XYZ(get_model(), P0->get_index()).get_coordinates() - core::XYZ(get_model(), P1->get_index()).get_coordinates();
   //algebra::Vector3D uvec = Dvec / Dvec.get_magnitude();
-
+  
+  // Then compute the distance needed to travel along the unit vector
   float h = 1/2 + (R1*R1-R0*R0)/(2 * Dvec.get_magnitude()*Dvec.get_magnitude());
-
-  //std::cout <<"H: " << h; //" | R0, R1 " << R0 << ", " << R1 << " |Dvec " << Dvec.get_magnitude() << std::endl;
 
   // First, if the loop lengths cannot span the two endpoints, return the center of the distance between them
   algebra::Vector3D center = core::XYZ(get_model(), P1->get_index()).get_coordinates() + h * Dvec;
-
-  //std::cout << " Res Endpoints: " << atom::Residue(get_model(), P0->get_index()) << " | " << atom::Residue(get_model(), P1->get_index()) << std::endl;
-
-  //std::cout << " P0;P1;cen: " << core::XYZ(get_model(), P0->get_index()).get_coordinates() << " | " << core::XYZ(get_model(), P1->get_index()).get_coordinates() << " || " << center << std::endl;
 
   return center;
 }
 
 double LoopPairDistanceRestraint::get_loop_distance(int a, int b) const {
-  
+  // Get the statistical potential difference for the maximum loop length
+  // between residues a and b
   int res_dist = abs(a - b);
   float mean_a;
   float sd_a;
@@ -130,12 +131,12 @@ double LoopPairDistanceRestraint::get_loop_distance(int a, int b) const {
   if(res_dist < 30) { 
     mean_a = hh_means_[res_dist-1]; sd_a = hh_sds_[res_dist-1];
   } else { 
+    // If loop is >30 residues, use the dist/residue for 30 residues
     mean_a = hh_means_[29]; sd_a = hh_sds_[29];
   }
 
+  // Max dist is mean plus a number of standard deviations above (hard-coded). 
   float dist = res_dist * (mean_a + n_sds_ * sd_a);
-
-  //std::cout << a << " " << b << " Res dist: " << res_dist << " Dist:" << dist << " Mean/SD: " << mean_a << "/" << sd_a << std::endl; 
 
   return dist;
 }
@@ -146,6 +147,11 @@ double LoopPairDistanceRestraint::unprotected_evaluate(DerivativeAccumulator *ac
   //IMP_CHECK_OBJECT(b_.get());
   IMP_CHECK_OBJECT(score_func_);
   IMP_OBJECT_LOG;
+  algebra::Vector3D a_center;
+  algebra::Vector3D b_center;
+  float offset_a;
+  float offset_b;
+  float center_dist;
 
   // Find if either or both residues have coordinates (using are_optimized as a proxy for this)
   bool a_opt = core::XYZ(get_model(), a_).get_coordinates_are_optimized();
@@ -153,32 +159,30 @@ double LoopPairDistanceRestraint::unprotected_evaluate(DerivativeAccumulator *ac
   double score;
 
   // Determine XL evaluation distance in all circumstances...this could be written much better
-  float distance = 1000;
+  float distance = 1000;  // model distance
   float loop_dist;
   float new_dist;
-  //std::cout << "||||---- resis:" << atom::Residue(get_model(), a_).get_index() << " " << atom::Residue(get_model(), b_).get_index() << std::endl;
 
-  // If both residues are structured, this is easy
+  // If both residues are structured, computing the model distance is easy
   if (a_opt==true and b_opt==true) {
     distance = core::get_distance(core::XYZ(get_model(), a_), core::XYZ(get_model(), b_));
-
+  
+    // If not this gets very ugly
   } else {
 
-    algebra::Vector3D a_center;
-    algebra::Vector3D b_center;
     algebra::Vector3D D_vec_a;
     algebra::Vector3D D_vec_b;
     Particles ps_a;
     Particles ps_b;
-    float offset_a;
-    float offset_b;
     float R0_a;
     float R0_b;
     float R1_a;
     float R1_b;
+    float alpha;
+    float avec;
     double pi_over_2 = IMP::PI * 0.5;
 
-
+    // If A is structured, then its center is 
     if (a_opt==true) {
       a_center = core::XYZ(get_model(), a_).get_coordinates();
     } else {
@@ -186,60 +190,92 @@ double LoopPairDistanceRestraint::unprotected_evaluate(DerivativeAccumulator *ac
       // Find particles of closest built residues to b_
       ps_a = get_closest_built_residue_particles(a_);
 
-      // Get sphere cap parameters
-      D_vec_a = core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates() - core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates(); // Vector between structured endpoints
-
-      R0_a = get_loop_distance(atom::Residue(ps_a[0]).get_index(), atom::Residue(get_model(), a_).get_index());
-      R1_a = get_loop_distance(atom::Residue(ps_a[1]).get_index(), atom::Residue(get_model(), a_).get_index());
-
-      // Center of sphere cap
-      if (R0_a > R1_a + D_vec_a.get_magnitude()){
-        a_center = core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates();
-      } else if (R1_a > R0_a + D_vec_a.get_magnitude()) {
-        a_center = core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates();
+      if (ps_a.size()==1){
+	  // If there is only one endpoint, then the center of the uncertainty sphere is this atom.
+	a_center = core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates();
+      } else if (ps_a.size()==0) {
+	  // If there are no endpoints found, then the crosslink endpoint is on a completely disordered chain.
+	  // What does this mean?  For now, return a constant value, however this is not optimal
+	  // However, the likelihood of a completely disordered chain is small.
+          return 10;
       } else {
-        a_center = get_sphere_cap_center(ps_a[0], ps_a[1], R0_a, R1_a);        
-      }
+      // Get sphere cap parameters
+        D_vec_a = core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates() - core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates(); // Vector between structured endpoints
+        
+        R0_a = get_loop_distance(atom::Residue(ps_a[0]).get_index(), atom::Residue(get_model(), a_).get_index());
+        R1_a = get_loop_distance(atom::Residue(ps_a[1]).get_index(), atom::Residue(get_model(), a_).get_index());
+      
+        // Compute Center of sphere cap
+        if (R0_a > R1_a + D_vec_a.get_magnitude()){
+          a_center = core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates();
+        } else if (R1_a > R0_a + D_vec_a.get_magnitude()) {
+          a_center = core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates();
+        } else {
+          a_center = get_sphere_cap_center(ps_a[0], ps_a[1], R0_a, R1_a);        
+        }
 
+      }
 
     }
 
+    //----------------------------------------
+    // Now we do the same as above for Particle B
     if (b_opt==true) {
       b_center = core::XYZ(get_model(), b_).get_coordinates();
     } else {
 
       // Find particles of closest built residues to b_
       ps_b = get_closest_built_residue_particles(b_);
+      //if both endpoints are in the same loop, it is satisfied - Do we really want to do this?
+      //if (ps_b[0]==ps_a[0] && ps_b[1]==ps_a[1]){return 0;}
+      //if (ps_b[1]==ps_a[0] && ps_b[0]==ps_a[1]){return 0;}
+
 
       // Get sphere cap parameters
-      D_vec_b = core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates() - core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates(); // Vector between structured endpoints
-      R0_b = get_loop_distance(atom::Residue(ps_b[0]).get_index(), atom::Residue(get_model(), b_).get_index());
-      R1_b = get_loop_distance(atom::Residue(ps_b[1]).get_index(), atom::Residue(get_model(), b_).get_index());
-      
-      // Center of uncertainty volume
-      if (R0_b > R1_b + D_vec_b.get_magnitude()){
-        b_center = core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates();
-      } else if (R1_b > R0_b + D_vec_b.get_magnitude()) {
-        b_center = core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates();
+      if (ps_b.size()==1) {
+	  b_center = core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates();
+      } else if (ps_b.size()==0) {
+	  return 10;
       } else {
-        b_center = get_sphere_cap_center(ps_b[0], ps_b[1], R0_b, R1_b);
+        D_vec_b = core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates() - core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates(); // Vector between structured endpoints
+      
+        R0_b = get_loop_distance(atom::Residue(ps_b[0]).get_index(), atom::Residue(get_model(), b_).get_index());
+        R1_b = get_loop_distance(atom::Residue(ps_b[1]).get_index(), atom::Residue(get_model(), b_).get_index());
+      
+        // Center of uncertainty volume
+        if (R0_b > R1_b + D_vec_b.get_magnitude()){
+          b_center = core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates();
+        } else if (R1_b > R0_b + D_vec_b.get_magnitude()) {
+          b_center = core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates();
+        } else {
+          b_center = get_sphere_cap_center(ps_b[0], ps_b[1], R0_b, R1_b);
+        }
       }
     }
 
-    // **********************************************************
+    // ------------------------------------------------------
+    // Now that we've computed the sphere cap centers, we can compute the model distance
     // Get vector from sc_center to other endpoint
     algebra::Vector3D xl_vec = a_center - b_center;
-    float center_dist = algebra::get_distance(a_center, b_center);
-
+    center_dist = algebra::get_distance(a_center, b_center);
+    
     // Now, subtract the distance from the center to the intersections of the sphere caps
+    // We also consider the loop lengths when a_ is not ordered
     if (a_opt==true){
       offset_a = 0;
-    } else {
-      // Get sphere cap angle
-      float alpha = std::acos((D_vec_a * xl_vec) / (D_vec_a.get_magnitude() * xl_vec.get_magnitude()));
-      //std::cout << "ACEN: " << a_center << " a: " << alpha << " " << D_vec_a << " " << xl_vec << " " << pi_over_2 << std::endl;
-      //std::cout << " ---dveca, xlvec " << D_vec_a.get_magnitude() << " " << xl_vec.get_magnitude() << " " << alpha << std::endl;
-      // If less than PI/2, use the R1 sphere cap
+      // If a_ only has one endpoint (is on a terminal loop) the loop distance is simple to compute
+    } else if (ps_a.size()==1){
+      offset_a = get_loop_distance(atom::Residue(ps_a[0]).get_index(), atom::Residue(get_model(), a_).get_index());
+    } else  {
+      //If a_ has two endpoints, then we must compute the angle between the XL vector and D vector to find where it
+      // intersects the sphere cap  
+      avec = (D_vec_b * xl_vec) / (D_vec_b.get_magnitude() * xl_vec.get_magnitude());
+      
+      // Sometimes floating point errors cause problems at the bounds
+      if (avec > 1.0){alpha=0;}
+      else if (avec < -1.0){alpha=3.14159;}
+      else{alpha = std::acos(avec);}
+
       if (D_vec_a.get_magnitude() > R1_a + R0_a){
         offset_a = 0;
       } else if (R0_a > R1_a + D_vec_a.get_magnitude()){
@@ -247,22 +283,24 @@ double LoopPairDistanceRestraint::unprotected_evaluate(DerivativeAccumulator *ac
       } else if (R1_a > R0_a + D_vec_a.get_magnitude()){
         offset_a = R1_a;
       } else if (alpha > pi_over_2) {
-        //std::cout << core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates() << std::endl;
         offset_a = calc_sphere_cap_distance(R1_a, algebra::get_distance(a_center, core::XYZ(get_model(), ps_a[1]->get_index()).get_coordinates()), alpha-pi_over_2);
       } else {
         offset_a = calc_sphere_cap_distance(R0_a, algebra::get_distance(a_center, core::XYZ(get_model(), ps_a[0]->get_index()).get_coordinates()), alpha);
       }
     }
 
-    // Now, subtract the distance from the center to the intersections of the sphere caps
+    // Now, we do the same for B
     if (b_opt==true){
       offset_b = 0;
-    } else {
+    } else if (ps_b.size()==1){
+      offset_b = get_loop_distance(atom::Residue(ps_b[0]).get_index(), atom::Residue(get_model(), b_).get_index());
+    } else  {
       // Get sphere cap angle
-      float alpha = std::acos((D_vec_b * xl_vec) / (D_vec_b.get_magnitude() * xl_vec.get_magnitude()));
-      //std::cout <<  "BCEN: " << b_center << " a: " << alpha << " " << D_vec_b << " " << xl_vec << std::endl;
-      //std::cout << " ---dvecb, xlvec " << D_vec_b.get_magnitude() << " " << xl_vec.get_magnitude() << " " << alpha << " " << R0_b << " " << R1_b << std::endl;
-      // If less than PI/2, use the R1 sphere cap
+      avec = (D_vec_b * xl_vec) / (D_vec_b.get_magnitude() * xl_vec.get_magnitude());
+      if (avec > 1.0){alpha=0;}
+      else if (avec < -1.0){alpha=3.14159;}
+      else{alpha = std::acos(avec);}
+      
       if (D_vec_b.get_magnitude() > R1_b + R0_b){
         offset_b = 0;
       } else if (R0_b > R1_b + D_vec_b.get_magnitude()){
@@ -271,19 +309,18 @@ double LoopPairDistanceRestraint::unprotected_evaluate(DerivativeAccumulator *ac
         offset_b = R1_b;
       } else if (alpha < pi_over_2) {
         offset_b = calc_sphere_cap_distance(R1_b, algebra::get_distance(b_center, core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates()), alpha);
-        //std::cout << "Radius: " << R1_b << " Chord dist: " << algebra::get_distance(b_center, core::XYZ(get_model(), ps_b[1]->get_index()).get_coordinates())  << std::endl;
       } else {
         offset_b = calc_sphere_cap_distance(R0_b, algebra::get_distance(b_center, core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates()), alpha-pi_over_2);
-        //std::cout << "Radius: " << R0_b << " Chord dist: " << algebra::get_distance(b_center, core::XYZ(get_model(), ps_b[0]->get_index()).get_coordinates())  << std::endl;
       }
     }
+    
+    // The model distance is finally computed as the distance between the sphere cap centers minus offsets due to the sphere cap surfaces
     distance = center_dist - offset_a - offset_b;
-    //std::cout << "---Cendist: " << center_dist << " offA: " << offset_a << " offB: " << offset_b << std::endl;   
   }
 
-  // Evaluate the score based on the shortest distance
+  // Evaluate the score based on this distance
   score = score_func_->evaluate(distance);
-  //std::cout << " || Score: " << score << std::endl; 
+  //std::cout << " || Score: " << score << " " << distance << " " << center_dist << " " << offset_a << " " << offset_b << " | " << a_opt << " " << b_opt << std::endl; 
   return score;
 }
 
