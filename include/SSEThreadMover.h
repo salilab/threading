56dadfa7551f0a8be6214447425bc8e5aa933123
saltzberg <saltzberg.salilab.org>
2020-06-03 14:32:01 -0700

    Add SSEThreadMover base class

---
+++ include/SSEThreadMover.h
@@ -0,0 +1,75 @@
+/**
+ *  \file IMP/threading/SSEThreadMover.h
+ *  \brief This mover updates the sequence assignment of a set of StructureElements
+ *  with respect to a given sequence hierarchy. This class is meant to be a base-class
+ *  for specific move types that modify specific SE keys.  
+ *  
+ *  THis object is not meant to be used as an actual mover.  do_propose() 
+ *  and do_reject() must be formulated for the specific move type in an inherited object
+ *
+ *  Copyright 2007-2020 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPTHREADING_SSETHREAD_MOVER_H
+#define IMPTHREADING_SSETHREAD_MOVER_H
+
+#include <IMP/threading/threading_config.h>
+#include <IMP/threading/StructureElement.h>
+#include <IMP/core/MonteCarloMover.h>
+#include <IMP/base_types.h>
+#include <IMP/exception.h>
+#include <IMP/atom/Hierarchy.h>
+#include <boost/random.hpp>
+//#include <IMP/statistics/statistics_config.h>
+//#include <IMP/atom/Residue.h>
+//#include <IMP/core/XYZ.h>
+IMPTHREADING_BEGIN_NAMESPACE
+
+//! A mover for modifying sequence assignment keys for structure elements in an SSETHread system
+//! from an IMP.threading.StructureElement.
+//! 
+//! Input is a list of structure_element indexes and the structural hierarchy particle index
+//!
+//! The class allows for easy random selection of a StructureElement and the modification of the 
+//! sequence hierarchy based on the keys from the StructureElement
+//!
+//! ####  This class cannot be used as is. ####  
+//! 
+//! Please inherit from it and define do_propose(), do_reject() and do_accept()
+//! to create the mover behavior you want
+
+class IMPTHREADINGEXPORT SSEThreadMover : public IMP::core::MonteCarloMover {
+  ParticleIndexes se_pis_;     // Structure Element particle indexes
+  ParticleIndex s_hier_pi_;  // Sequence root hierarchy node particle index
+
+  void initialize(Model *m, ParticleIndexes se_pis, ParticleIndex s_hier_pi);
+
+ public:
+
+ SSEThreadMover(Model *m, ParticleIndexes se_pis, ParticleIndex s_hier_pi);
+
+ void transform_coordinates(ParticleIndex se_pi);
+ void zero_coordinates(ParticleIndex se_pi);
+
+StructureElement get_random_structure_element_particle() {
+  // Get a random particle index from the list
+  ::boost::uniform_int<> rand(0, se_pis_.size() - 1);
+  StructureElement ret = StructureElement(get_model(), se_pis_[int(rand(random_number_generator))]);  
+  return ret;
+}
+
+ protected:
+
+  // These functions must be defined in classes that inherit this object.
+  virtual ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE;
+  virtual IMP::core::MonteCarloMoverResult do_propose() IMP_OVERRIDE;
+  virtual void do_reject() IMP_OVERRIDE;
+  
+  IMP_OBJECT_METHODS(SSEThreadMover);
+
+};
+
+IMPTHREADING_END_NAMESPACE
+
+#endif /* IMPTHREADING_SSETHREAD_MOVER_H */
---
+++ src/SSEThreadMover.cpp
@@ -0,0 +1,103 @@
+/**
+ *  \file SSEThreadMover.cpp  \brief A modifier to a Fragment object that perturbs 
+ *     the start residue by an integer value.
+ *
+ *  Copyright 2007-2020 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/threading/SSEThreadMover.h>
+#include <IMP/threading/StructureElement.h>
+
+IMPTHREADING_BEGIN_NAMESPACE
+
+namespace {
+std::string get_structure_element_mover_name() {
+  return "SSEThreadMover-BASE";
+}
+}
+
+void SSEThreadMover::initialize(Model *m, ParticleIndexes se_pis, ParticleIndex s_hier_pi) {
+  se_pis_ = se_pis;
+  s_hier_pi_ = s_hier_pi;
+}
+
+SSEThreadMover::SSEThreadMover(Model *m, ParticleIndexes se_pis, ParticleIndex s_hier_pi)
+    : IMP::core::MonteCarloMover(m, get_structure_element_mover_name()) {
+  initialize(m, se_pis, s_hier_pi);
+}
+
+IMP::core::MonteCarloMoverResult SSEThreadMover::do_propose() {
+      return IMP::core::MonteCarloMoverResult(atom::Hierarchy(get_model(), s_hier_pi_).get_children(),1.0);
+}
+
+
+void SSEThreadMover::do_reject() {
+  IMP_OBJECT_LOG;
+}
+
+void SSEThreadMover::transform_coordinates(ParticleIndex se_pi){
+  IMP_USAGE_CHECK(StructureElement().get_is_setup(get_model(), se_pi), "Particle is not set up as a StructureElement");
+  threading::StructureElement se(get_model(), se_pi);
+
+  // If the SR has been changed to zero, then zero out these coordinates
+  if (se.get_start_res()==0){
+    return;
+  }
+  
+  // We want the sequence root hierarchy
+  atom::Hierarchy h(get_model(), s_hier_pi_);
+
+  // Create a selection to find the residues we wish to 
+  // add these coordinates to.
+  atom::Selection sel(h);
+ 
+  sel.set_residue_indexes(se.get_resindex_list());
+  
+  // Select these particles
+  ParticlesTemp new_resis = sel.get_selected_particles();
+  
+  algebra::Vector3Ds new_coords = se.get_coordinates();
+
+  // Flip the order of newresis if polarity is -1
+  if(se.get_polarity()==-1)
+   std::reverse(new_resis.begin(), new_resis.end());
+
+  // For polarity, assign forward, for polarity = -1, assign backwards
+  for (unsigned int i=0; i<se.get_resindex_list().size(); i++) {
+    core::XYZ(new_resis[i]).set_coordinates(new_coords[i]);
+    // Set this as a flag for evaluation or not
+    core::XYZ(new_resis[i]).set_coordinates_are_optimized(true);    
+  }; 
+}
+
+
+void SSEThreadMover::zero_coordinates(ParticleIndex se_pi){
+  // Zero out the coordinates from this StructureElement
+  IMP_USAGE_CHECK(StructureElement().get_is_setup(get_model(), se_pi), "Particle is not set up as a StructureElement");
+  threading::StructureElement se(get_model(), se_pi);
+  atom::Hierarchy h(get_model(), s_hier_pi_);
+  atom::Selection sel(h);
+  sel.set_residue_indexes(se.get_resindex_list());
+  ParticlesTemp oldsel = sel.get_selected_particles();
+  for (unsigned int i=0; i<oldsel.size();i++){
+    core::XYZ coord(oldsel[i]);
+    
+    coord.set_coordinates(algebra::Vector3D(0,0,0));
+    //Set this as a flag for evaluation or not
+    coord.set_coordinates_are_optimized(false);
+  };
+}
+
+ModelObjectsTemp SSEThreadMover::do_get_inputs() const {
+  ModelObjectsTemp ret;
+ 
+  ret.push_back(get_model()->get_particle(s_hier_pi_));
+  for (unsigned int i=0; i<se_pis_.size(); i++){
+    ret.push_back(get_model()->get_particle(se_pis_[i]));
+  } 
+  
+  return ret;
+}
+
+IMPTHREADING_END_NAMESPACE
