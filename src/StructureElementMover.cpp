6d438a6bb240bdbe5b33636722b37f40c6bf3aba
dibyendu92 <dmondal@usc.edu>
2021-08-23 12:49:43 -0700

    added more checks to avoid overflowing the StartRes key during the monte carlo moves

--- src/StructureElementMover.cpp
+++ src/StructureElementMover.cpp
@@ -38,21 +38,26 @@ IMP::core::MonteCarloMoverResult StructureElementMover::do_propose() {
   
   // 1) get the original key values
   orig_key_values_ = se.get_all_key_values();
-
+  int seq_len = atom::Hierarchy(get_model(), s_hier_pi_).get_children().size();
   int r;
   
   //  Zero out the coordinates in the Sequence hierarchy
   zero_coordinates();
-
   // Propose move to start_res
   if (se.get_start_res_is_optimized()) {
     r = statistics::internal::random_int(2) * 2 - 1;
     int new_start_res = orig_key_values_[0] - r;
-    se.set_start_res_key(new_start_res);
+      //std::cout << "new_start_res + length : " << (new_start_res + orig_key_values_[2]) << std::endl;
+    if (new_start_res > 0 && (new_start_res + orig_key_values_[2]) <= seq_len-2) {
+        // new_start_res = orig_key_values_[0];
+        // std::cout << "new_start_res: " << new_start_res << std::endl;
+        se.set_start_res_key(new_start_res);
+    };
   };
 
   // Propose move to polarity
   if (se.get_polarity_is_optimized() && rand() % 100 < pct_flip_) {
+      //std::cout << "tried polarity change and flipped it"<< std::endl;
     se.flip_polarity_key();
   };
 
@@ -60,9 +65,11 @@ IMP::core::MonteCarloMoverResult StructureElementMover::do_propose() {
   if (se.get_length_is_optimized() && rand() % 100 < pct_length_) {
     r = statistics::internal::random_int(2) * 2 - 1;
     int new_length = orig_key_values_[2] - r;
-    if ( new_length >= 1 && new_length + se.get_offset() <= se.get_number_of_coordinates()) {
-      se.set_length_key(new_length);
-    } 
+    if ( new_length >= 1 && (new_length + se.get_offset()) <= se.get_number_of_coordinates() && (new_length + se.get_start_res()) <= seq_len-2) {
+        //std::cout << "proposed new length "<< new_length << " current start res " << se.get_start_res() << " where seq_len is "  << seq_len << std::endl;
+        se.set_length_key(new_length);
+    }
+      //std::cout << "changed total length "<< (new_length + se.get_start_res()) << " where seq_len is "  << seq_len << std::endl;
   };
 
   // Propose move to offset
@@ -70,13 +77,13 @@ IMP::core::MonteCarloMoverResult StructureElementMover::do_propose() {
     r = statistics::internal::random_int(2) * 2 - 1;
     int new_offset = orig_key_values_[3] - r;
     if ( new_offset >= 0 && new_offset <= se.get_max_offset() ) {
-      se.set_offset_key(new_offset);
+        //std::cout << "offset changed and set to less than max_offset"<< std::endl;
+        se.set_offset_key(new_offset);
     }
   };
-
   // With the new key values, move the SE coordiantes to the sequence hierarchy
-  transform_coordinates();
 
+  transform_coordinates();
   return IMP::core::MonteCarloMoverResult(h.get_children(), 1.0);
 }
 
@@ -106,20 +113,20 @@ void StructureElementMover::transform_coordinates(){
   if (se.get_start_res()==0){
     return;
   }
-  
+
   // We want the sequence root hierarchy
   atom::Hierarchy h(get_model(), s_hier_pi_);
 
   // Create a selection to find the residues we wish to 
   // add these coordinates to.
   atom::Selection sel(h);
- 
+
   sel.set_residue_indexes(se.get_resindex_list());
   sel.set_chain(se.get_chain());
 
   // Select these particles
   ParticlesTemp new_resis = sel.get_selected_particles();
-  
+
   algebra::Vector3Ds new_coords = se.get_coordinates();
 
   // Flip the order of newresis if polarity is -1
@@ -127,10 +134,16 @@ void StructureElementMover::transform_coordinates(){
    std::reverse(new_resis.begin(), new_resis.end());
 
   // For polarity, assign forward, for polarity = -1, assign backwards
-  for (unsigned int i=0; i<se.get_resindex_list().size(); i++) {
+
+
+  // TO AVOID SEG FAULT Dibyendu replaced se.get_resindex_list() to new_resis.size()
+  //for (unsigned int i=0; i<se.get_resindex_list().size(); i++) {
+    for (unsigned int i=0; i<new_resis.size(); i++) {
+
     core::XYZ(new_resis[i]).set_coordinates(new_coords[i]);
+
     // Set this as a flag for evaluation or not
-    core::XYZ(new_resis[i]).set_coordinates_are_optimized(true);    
+    core::XYZ(new_resis[i]).set_coordinates_are_optimized(true);
   }; 
 }
 
